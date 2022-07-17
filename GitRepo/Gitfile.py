180c2493871873f454a7263badbc0a378c815cb0
Prince <Princekumar@gmail.com>
2022-07-18 00:40:45 +0530

    3rd Commit after modifying 2nd commit

--- GitRepo/Gitfile.py
+++ GitRepo/Gitfile.py
@@ -6,7 +6,8 @@ git init
 git clone
 git add
 git add -A
-git commit
+git rm --cached filename "This command will Move your file again into Unstaged Area"
+git commit "This command will Move your file from Untracked to staged Area"
 git commit -a -m "This Cmd will skipp the Stageing Area and commit directly"
 git diff
 git reset
