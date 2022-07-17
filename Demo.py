4e109a0908b4f2b91f64510315134f0657387db4
root <root@ip-172-31-85-236.ec2.internal>
2022-07-17 18:04:42 +0000

    All Git Commands

--- Demo.py
+++ Demo.py
@@ -1,5 +1,11 @@
-#Simple Addition program
-a = 10
-b = 20
-print("Addition is :" a + b )
+#GIT ALL Commands
+git init #Initialize the Git repo
+git add # Add the file from Local To Working or Staging Area
+git status #Check Status of File whether that has been Added in to Staging or Not
+git log #Check how many commit has been done by Devloper
+git show #Check the specific commit details on the basis of Commit ID
+git remot add origin master #ADD the file from Staging to Central Repo
+git push origin master # Push the file from Staging to Remot/Central Repo
+git pull # Pull the file from remot/Central Repo to Local Repo
+
 
